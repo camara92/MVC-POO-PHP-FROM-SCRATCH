3857377607df0c42bc0d292b764ad4658f432d0d
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-17 12:32:50 +0200

    views and controllers

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
      switch($_GET['page']){
          case 'accueil': require_once ("./view/home.view.php");
          break; 
+         case 'games': require_once ("./view/games.view.php");
+         break; 
+         case 'users': require_once ("./view/users.view.php");
+         break; 
         //  case 'prout': echo "Daouda";
         //  break; 
      }
--- view/base.html.php
+++ view/base.html.php
@@ -12,7 +12,7 @@
 
 <body>
     <nav class="navbar navbar-expand navbar-dark bg-primary">
-        <a class="navbar-brand" href="index.php">Game-X</a>
+        <a class="navbar-brand" href="accueil">Game-X</a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -21,14 +21,14 @@
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Accueil</a>
+                    <a class="nav-link" href="accueil">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="games.php">Jeux</a>
+                    <a class="nav-link" href="games">Jeux</a>
 
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="users.php">Joueurs </a>
+                    <a class="nav-link" href="users">Joueurs </a>
 
                 </li>
                
