06ccd7f20c4ca55d77d2ba1b3e324174a27aa562
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-17 10:47:42 +0200

    Création des dossiers view-controller et models => basculement contenu index vers view.php

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?php  ob_start();?>
-
-
-
-<p>Bienvenue sur notre store !</p>
-
-
-
-
 <?php
-
-$content =ob_get_clean();
-$title = "Bienvenue chez Game-X";
-require_once "base.html.php";
-
+    require_once ("./view/view.php");
 ?>
---
+++ view/view.php
@@ -0,0 +1,16 @@
+<?php  ob_start();?>
+
+
+
+<p>Bienvenue sur notre store !</p>
+
+
+
+
+<?php
+
+$content =ob_get_clean();
+$title = "Bienvenue chez Game-X";
+require_once "base.html.php";
+
+?>
