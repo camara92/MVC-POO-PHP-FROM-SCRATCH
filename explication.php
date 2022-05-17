4543fe3b13db162f43153e8f0dbd88a7a947741f
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-17 16:24:36 +0200

    ajout du sous routage slide 47 du projet

--- explication.php
+++ explication.php
@@ -46,4 +46,40 @@ require_once "base.html.php";
 
 
 // var_dump($exploz); 
+
+
+
+define("URL" , str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https" : "http") . 
+"://".$_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] ));
+
+require_once "controller/GameController.php";
+$gameController = new GameController();
+
+if(empty($_GET['page'])){
+    require_once "view/home.view.php";
+}else{
+    $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL) );
+    switch ($url[0]) {
+        case 'accueil': require_once "view/home.view.php";
+        break;
+
+        case 'games': 
+            if(empty($url[1])){
+                $gameController->displayGames();
+            }elseif($url[1] == "add"){
+                echo "Ajout d'un jeu";
+            }elseif($url[1] == "edit"){
+                echo "Modifier un jeu";
+            }elseif($url[1] == "delete"){
+                echo "Supprimer un jeu";
+            }
+        break;
+
+        
+        case 'users': require_once "view/users.view.php";
+        break;
+    }
+}
+
+
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-define("URL" , str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https" : "http") . 
-"://".$_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] ));
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") .
+    "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']));
 //debuter  la racine du projet : explication amateur 
 // echo URL;
 require_once "controller/GameController.php";
@@ -10,15 +10,25 @@ $gameController = new GameController();
 if (empty($_GET['page'])) {
     require_once "view/home.view.php";
 } else {
-    $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);
-    var_dump($url); 
-    
-    switch ($_GET['page']) {
+
+    $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
+    // var_dump($url); 
+
+
+    switch ($url[0]) {
         case 'accueil':
             require_once("./view/home.view.php");
             break;
         case 'games':
-            $gameController->displayGames();
+            if (empty($url[1])) {
+                $gameController->displayGames();
+            } elseif (empty($url[1] === "add")) {
+                echo "Créer un jeu";
+            } elseif (empty($url[1] === "edit")) {
+                echo "CModifier un jeu";
+            } elseif (empty($url[1] === "delete")) {
+                echo "Supprimer un jeu";
+            }
             break;
             //  case 'users': require_once ("./view/users.view.php");
             //  break; 
